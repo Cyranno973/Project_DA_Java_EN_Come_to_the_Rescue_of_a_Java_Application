f125131fa398f0bcb5b37472dbe28807622c62a5
Cyranno <gs.gouin@gmail.com>
2020-03-27 22:19:41 +0100

    utilisation de la class CountSymptom

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,5 +1,6 @@
 package com.hemebiotech.analytics;
 
+import com.hemebiotech.analytics.count.CountSymptom;
 import com.hemebiotech.analytics.read.ReadSymptomDataFromFile;
 
 import java.io.BufferedReader;
@@ -8,8 +9,7 @@ import java.io.FileWriter;
 import java.util.*;
 
 public class AnalyticsCounter {
-    //On declare une HashMap Clé/Valeur = symptoms/occurrence
-    static Map<String, Integer> symptomsCounter = new HashMap<>();
+
     public static void main(String args[]) throws Exception {
         //Etape1 On lit le fichier symptoms.txt et stock dans une list de String allSymptoms
 
@@ -18,13 +18,9 @@ public class AnalyticsCounter {
 
         //Etape2 On parcours le fichier en comptant les symptomes avec la map
 
-        for (String symptom : allSymptoms) {
-            if (symptomsCounter.containsKey(symptom)) {
-                symptomsCounter.put(symptom, symptomsCounter.get(symptom) + 1);
-            } else {
-                symptomsCounter.put(symptom, 1);
-            }
-        }
+        CountSymptom counter = new CountSymptom();
+        Map<String, Integer> symptomsCounter = counter.count(allSymptoms);
+
         //Etape3 On range dans l'ordre alphabetique les symptomes
         List<String> symptoms = new ArrayList<>(symptomsCounter.keySet());
         Collections.sort(symptoms);
--- Project02Eclipse/src/com/hemebiotech/analytics/count/CountSymptom.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/count/CountSymptom.java
@@ -1,4 +1,26 @@
 package com.hemebiotech.analytics.count;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class CountSymptom {
+
+    /**
+     * Cette methode compt les differents symptoms
+     * @param symptoms
+     * @return
+     */
+    public Map<String, Integer> count(List<String> symptoms){
+       Map<String, Integer> symptomsCounter = new HashMap<>();
+
+        for (String symptom : symptoms) {
+            if (symptomsCounter.containsKey(symptom)) {
+                symptomsCounter.put(symptom, symptomsCounter.get(symptom) + 1);
+            } else {
+                symptomsCounter.put(symptom, 1);
+            }
+        }
+        return symptomsCounter;
+    }
 }
